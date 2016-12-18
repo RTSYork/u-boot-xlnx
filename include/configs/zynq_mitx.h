ec2e0cf128108563f81b9996c6551c917f5de94e
Russell Joyce <russell@russellj.co.uk>
2016-12-18 20:19:13 +0000

    Added config and device tree files for Zynq Mini-ITX board

---
+++ include/configs/zynq_mitx.h
@@ -0,0 +1,15 @@
+/*
+ * (C) Copyright 2013 Xilinx, Inc.
+ *
+ * Configuration for Avnet Zynq Mini-ITX Board
+ * See zynq-common.h for Zynq common configs
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQ_MITX_H
+#define __CONFIG_ZYNQ_MITX_H
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_ZYNQ_MITX_H */
